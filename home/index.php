d5e2c91e1beb17abfcdf45ba284eda7babb69591
Frank <anonymousplaysminecraft1@gmail.com>
2016-09-25 13:55:48 -0500

    General updates
    Styling improvements
    Tiny cleanup

---
+++ home/index.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>BlizzFist Studious | Home</title>
+		<link rel="stylesheet" type="text/css" href="../stylesheet/style.css"/>
+	</head>
+	<body>
+		<div class="section uppercenter">
+			<div class="headerbox center boxshadow ">
+				<span class="text textAnim">BlizzFist Studios</span>
+			</div>
+				<a class="nav hide" href="#">Home</a>
+				<a class="nav hide" href="#">About</a>
+		</div>
+		<div class="section">
+				<div class="leftbox boxshadow">
+					<a class="nav hide" href="#">Home</a>
+					<a class="nav hide" href="#">About</a>
+				</div>
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
 	session_start();
+	header("Location: home");
 ?>
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="stylesheet/style.css"/>
-	</head>
-	<body>
-		<div class="section uppercenter">
-			<div class="headerbox center boxshadow ">
-				<span class="text textAnim">BlizzFist Studios</span>
-			</div>
-		</div>
-		<div class="section">
-				<div class="leftbox boxshadow">
-					<a class="nav" href="/">Home</a>
-					<a class="nav" href="/">About</a>
-				</div>
-		</div>
-	</body>
-</html>
