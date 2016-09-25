936f042634269fa11d046b3fd4ffbf6690092c11
Frank <anonymousplaysminecraft1@gmail.com>
2016-09-25 10:42:51 -0500

    Website commit
    Commit website code as is

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="stylesheet/style.css"/>
+	</head>
+	<body>
+		<div class="section uppercenter">
+			<div class="headerbox center boxshadow ">
+				<span class="text textAnim">BlizzFist Studios</span>
+			</div>
+		</div>
+		<div class="section">
+				<div class="leftbox boxshadow">
+					<a class="nav" href="/">Home</a>
+					<a class="nav" href="/">About</a>
+				</div>
+		</div>
+	</body>
+</html>
